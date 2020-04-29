c6994e010a2b0a88ab13d62c03ed3ca457127c37
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-29 05:43:26 +0300

    Add 'r' module with 'Packed Hilbert R-tree' implementation

--- locus/core/utils.py
+++ locus/core/utils.py
@@ -5,6 +5,49 @@ from locus.hints import (Coordinate,
                          Point)
 
 
+def ceil_division(left: int, right: int) -> int:
+    return -(-left // right)
+
+
+HILBERT_SQUARE_SIZE = 2 ** 16
+HILBERT_MAX_COORDINATE = HILBERT_SQUARE_SIZE - 1
+
+
+def to_hilbert_index(x: int, y: int) -> int:
+    # based on https://github.com/rawrunprotected/hilbert_curves
+    assert 0 <= x <= HILBERT_MAX_COORDINATE
+    assert 0 <= y <= HILBERT_MAX_COORDINATE
+    a = x ^ y
+    b = HILBERT_MAX_COORDINATE ^ a
+    c, d = (HILBERT_MAX_COORDINATE ^ (x | y),
+            x & (y ^ HILBERT_MAX_COORDINATE))
+    a, b, c, d = (a | (b >> 1),
+                  (a >> 1) ^ a,
+                  ((c >> 1) ^ (b & (d >> 1))) ^ c,
+                  ((a & (c >> 1)) ^ (d >> 1)) ^ d)
+    a, b, c, d = (((a & (a >> 2)) ^ (b & (b >> 2))),
+                  ((a & (b >> 2)) ^ (b & ((a ^ b) >> 2))),
+                  c ^ ((a & (c >> 2)) ^ (b & (d >> 2))),
+                  d ^ ((b & (c >> 2)) ^ ((a ^ b) & (d >> 2))))
+    a, b, c, d = (((a & (a >> 4)) ^ (b & (b >> 4))),
+                  ((a & (b >> 4)) ^ (b & ((a ^ b) >> 4))),
+                  c ^ ((a & (c >> 4)) ^ (b & (d >> 4))),
+                  d ^ ((b & (c >> 4)) ^ ((a ^ b) & (d >> 4))))
+    c ^= ((a & (c >> 8)) ^ (b & (d >> 8)))
+    d ^= ((b & (c >> 8)) ^ ((a ^ b) & (d >> 8)))
+    a, b = c ^ (c >> 1), d ^ (d >> 1)
+    i0 = x ^ y
+    i1 = b | (HILBERT_MAX_COORDINATE ^ (i0 | a))
+    return ((interleave(i1) << 1) | interleave(i0)) >> 0
+
+
+def interleave(value: int) -> int:
+    value = (value | (value << 8)) & 0x00FF00FF
+    value = (value | (value << 4)) & 0x0F0F0F0F
+    value = (value | (value << 2)) & 0x33333333
+    return (value | (value << 1)) & 0x55555555
+
+
 def linear_distance(left: Coordinate, right: Coordinate) -> Coordinate:
     return abs(left - right)
 
@@ -18,3 +61,22 @@ def point_in_interval(point: Point, interval: Interval) -> bool:
     return all(min_coordinate <= point_coordinate <= max_coordinate
                for point_coordinate, (min_coordinate,
                                       max_coordinate) in zip(point, interval))
+
+
+def distance_to_planar_interval(point: Point,
+                                interval: Interval) -> Coordinate:
+    x, y = point
+    (min_x, max_x), (min_y, max_y) = interval
+    dx = distance_to_linear_interval(x, min_x, max_x)
+    dy = distance_to_linear_interval(y, min_y, max_y)
+    return dx * dx + dy * dy
+
+
+def distance_to_linear_interval(coordinate: Coordinate,
+                                min_coordinate: Coordinate,
+                                max_coordinate: Coordinate) -> Coordinate:
+    return (min_coordinate - coordinate
+            if coordinate < min_coordinate
+            else (coordinate - max_coordinate
+                  if coordinate > max_coordinate
+                  else 0))
---
+++ locus/r.py
@@ -0,0 +1,232 @@
+from math import floor
+from functools import reduce
+from typing import (Iterator,
+                    List,
+                    Optional,
+                    Sequence,
+                    Tuple,
+                    Type)
+
+from prioq.base import PriorityQueue
+from reprit.base import generate_repr
+
+from .core.utils import (HILBERT_MAX_COORDINATE,
+                         ceil_division,
+                         distance_to_planar_interval,
+                         to_hilbert_index)
+from .hints import (Coordinate,
+                    Interval,
+                    Point)
+
+Item = Tuple[int, Interval]
+
+
+class Node:
+    __slots__ = 'index', 'interval', 'children'
+
+    def __init__(self,
+                 index: int,
+                 interval: Interval,
+                 children: Optional[Sequence['Node']]) -> None:
+        self.index = index
+        self.interval = interval
+        self.children = children
+
+    __repr__ = generate_repr(__init__)
+
+    @property
+    def item(self) -> Item:
+        return self.index, self.interval
+
+    def distance_to_point(self, point: Point) -> Coordinate:
+        return distance_to_planar_interval(point, self.interval)
+
+
+def _create_root(intervals: Sequence[Interval],
+                 max_children: int,
+                 node_cls: Type[Node] = Node) -> Node:
+    intervals_count = len(intervals)
+    nodes = [node_cls(index, interval, None)
+             for index, interval in enumerate(intervals)]
+    interval = reduce(_merge_intervals, intervals)
+    if intervals_count <= max_children:
+        # only one node, skip sorting and just fill the root box
+        return node_cls(len(nodes), interval, nodes)
+    else:
+        (min_x, max_x), (min_y, max_y) = interval
+
+        def node_key(node: Node,
+                     double_tree_delta_x: Coordinate = 2 * (max_x - min_x),
+                     double_tree_delta_y: Coordinate = 2 * (max_y - min_y),
+                     double_tree_min_x: Coordinate = 2 * min_x,
+                     double_tree_min_y: Coordinate = 2 * min_y) -> int:
+            (min_x, max_x), (min_y, max_y) = node.interval
+            return to_hilbert_index(floor(HILBERT_MAX_COORDINATE
+                                          * (min_x + max_x - double_tree_min_x)
+                                          / double_tree_delta_x),
+                                    floor(HILBERT_MAX_COORDINATE
+                                          * (min_y + max_y - double_tree_min_y)
+                                          / double_tree_delta_y))
+
+        _presort([node_key(node) for node in nodes], nodes,
+                 0, intervals_count - 1, max_children)
+        nodes_count = step = len(intervals)
+        levels_limits = [nodes_count]
+        while True:
+            step = ceil_division(step, max_children)
+            if step == 1:
+                break
+            nodes_count += step
+            levels_limits.append(nodes_count)
+        start = 0
+        for level_limit in levels_limits:
+            while start < level_limit:
+                stop = min(start + max_children, level_limit)
+                children = nodes[start:stop]
+                nodes.append(node_cls(len(nodes),
+                                      reduce(_merge_intervals,
+                                             [child.interval
+                                              for child in children]),
+                                      children))
+                start = stop
+        return nodes[-1]
+
+
+def _node_to_hilbert_index(node, double_min_x, double_min_y,
+                           double_width, double_height) -> int:
+    return to_hilbert_index(floor(HILBERT_MAX_COORDINATE
+                                  * ((node.min_x + node.max_x - double_min_x)
+                                     / double_width)),
+                            floor(HILBERT_MAX_COORDINATE
+                                  * (node.min_y + node.max_y - double_min_y)
+                                  / double_height))
+
+
+class Tree:
+    def __init__(self,
+                 intervals: Sequence[Interval],
+                 *,
+                 max_children: int = 16,
+                 node_cls: Type[Node] = Node) -> None:
+        self._intervals = intervals
+        self._max_children = max_children
+        self._root = _create_root(intervals, max_children, node_cls)
+
+    __repr__ = generate_repr(__init__)
+
+    @property
+    def intervals(self) -> Sequence[Interval]:
+        return self._intervals
+
+    @property
+    def node_cls(self) -> Type[Node]:
+        return type(self._root)
+
+    @property
+    def max_children(self) -> int:
+        return self._max_children
+
+    def find_interval_indices(self, interval: Interval) -> List[int]:
+        return [index for index, _ in self._find_interval_items(interval)]
+
+    def find_interval_intervals(self, interval: Interval) -> List[Interval]:
+        return [interval
+                for _, interval in self._find_interval_items(interval)]
+
+    def find_interval_items(self, interval: Interval) -> List[Item]:
+        return list(self._find_interval_items(interval))
+
+    def _find_interval_items(self, interval: Interval) -> Iterator[Item]:
+        queue = [self._root]
+        while queue:
+            node = queue.pop()
+            if _interval_does_not_contain(interval, node.interval):
+                continue
+            if node.children is None:
+                yield node.item
+            else:
+                for child in node.children:
+                    if _interval_does_not_contain(interval, child.interval):
+                        continue
+                    if child.children is None:
+                        yield child.item
+                    else:
+                        queue.extend(child.children)
+
+    def n_nearest_indices(self, n: int, point: Point) -> Sequence[int]:
+        return ([index for index, _ in self.n_nearest_items(n, point)]
+                if n < len(self._intervals)
+                else range(len(self._intervals)))
+
+    def n_nearest_intervals(self, n: int, point: Point) -> Sequence[Interval]:
+        return ([interval for _, interval in self.n_nearest_items(n, point)]
+                if n < len(self._intervals)
+                else self._intervals)
+
+    def n_nearest_items(self, n: int, point: Point) -> List[Item]:
+        queue = PriorityQueue((0, 0, self._root))
+        items = []
+        while queue:
+            node = queue.pop()[2]
+            for child in node.children:
+                queue.push((child.distance_to_point(point),
+                            -child.index - 1
+                            if child.children is None
+                            else child.index,
+                            child))
+            while queue and queue.peek()[1] < 0:
+                items.append(queue.pop()[2].item)
+                if len(items) == n:
+                    queue.clear()
+                    return items
+        queue.clear()
+        return items
+
+
+def _interval_does_not_contain(goal: Interval, test: Interval) -> bool:
+    (goal_min_x, goal_max_x), (goal_min_y, goal_max_y) = goal
+    (test_min_x, test_max_x), (test_min_y, test_max_y) = test
+    return (test_max_x < goal_min_x or test_min_x > goal_max_x
+            or test_max_y < goal_min_y or test_min_y > goal_max_y)
+
+
+def _presort(keys: List[int],
+             nodes: List[Node],
+             start: int,
+             stop: int,
+             node_size: int) -> None:
+    if start // node_size >= stop // node_size:
+        return
+    pivot_index = _partition(keys, nodes, start, stop)
+    _presort(keys, nodes, start, pivot_index, node_size)
+    _presort(keys, nodes, pivot_index + 1, stop, node_size)
+
+
+def _partition(keys: List[int],
+               nodes: List[Node],
+               start: int,
+               stop: int) -> int:
+    pivot = keys[(start + stop) // 2]
+    start -= 1
+    stop += 1
+    while True:
+        while True:
+            start += 1
+            if pivot <= keys[start]:
+                break
+        while True:
+            stop -= 1
+            if keys[stop] <= pivot:
+                break
+        if start >= stop:
+            break
+        keys[start], keys[stop], nodes[start], nodes[stop] = (
+            keys[stop], keys[start], nodes[stop], nodes[start])
+    return stop
+
+
+def _merge_intervals(left: Interval, right: Interval) -> Interval:
+    (left_min_x, left_max_x), (left_min_y, left_max_y) = left
+    (right_min_x, right_max_x), (right_min_y, right_max_y) = right
+    return ((min(left_min_x, right_min_x), max(left_max_x, right_max_x)),
+            (min(left_min_y, right_min_y), max(left_max_y, right_max_y)))
