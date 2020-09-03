0a5d61a9019af0c6694c4b062901922fd7313c48
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-03 17:08:17 +0500

    Complete functions names for 'segment' module

--- locus/core/segment.py
+++ locus/core/segment.py
@@ -12,6 +12,10 @@ from locus.hints import (Coordinate,
 from . import interval as _interval
 
 
+def distance_to(left: Segment, right: Segment) -> Coordinate:
+    return sqrt(_squared_distance_to(left, right))
+
+
 def distance_to_interval(segment: Segment, interval: Interval) -> Coordinate:
     start, end = segment
     if (_interval.contains_point(interval, start)
@@ -27,6 +31,10 @@ def distance_to_interval(segment: Segment, interval: Interval) -> Coordinate:
                                                             min_y)))
 
 
+def distance_to_point(segment: Segment, point: Point) -> Coordinate:
+    return sqrt(_squared_distance_to_point(segment, point))
+
+
 def _distance_to_non_degenerate_interval(segment: Segment,
                                          max_x: Coordinate,
                                          max_y: Coordinate,
@@ -34,46 +42,44 @@ def _distance_to_non_degenerate_interval(segment: Segment,
                                          min_y: Coordinate) -> Coordinate:
     bottom_left = min_x, min_y
     bottom_right = max_x, min_y
-    bottom_side_distance = squared_distance_to(segment,
-                                               (bottom_left, bottom_right))
+    bottom_side_distance = _squared_distance_to(segment,
+                                                (bottom_left, bottom_right))
     if not bottom_side_distance:
         return bottom_side_distance
     top_right = max_x, max_y
-    right_side_distance = squared_distance_to(segment,
-                                              (bottom_right, top_right))
+    right_side_distance = _squared_distance_to(segment,
+                                               (bottom_right, top_right))
     if not right_side_distance:
         return right_side_distance
     top_left = min_x, max_y
-    top_side_distance = squared_distance_to(segment, (top_left, top_right))
+    top_side_distance = _squared_distance_to(segment, (top_left, top_right))
     if not top_side_distance:
         return top_side_distance
-    left_side_distance = squared_distance_to(segment, (bottom_left, top_left))
+    left_side_distance = _squared_distance_to(segment, (bottom_left, top_left))
     return (sqrt(min(bottom_side_distance, right_side_distance,
                      left_side_distance, top_side_distance))
             if left_side_distance
             else left_side_distance)
 
 
-def distance_to(left: Segment, right: Segment) -> Coordinate:
-    return sqrt(squared_distance_to(left, right))
+def _robust_divide(dividend: Coordinate, divisor: Coordinate) -> Coordinate:
+    return (Fraction(dividend, divisor)
+            if isinstance(dividend, int)
+            else dividend / divisor)
 
 
-def distance_to_point(segment: Segment, point: Point) -> Coordinate:
-    return sqrt(squared_distance_to_point(segment, point))
-
-
-def squared_distance_to(left: Segment, right: Segment) -> Coordinate:
+def _squared_distance_to(left: Segment, right: Segment) -> Coordinate:
     left_start, left_end = left
     right_start, right_end = right
-    return (min(squared_distance_to_point(left, right_start),
-                squared_distance_to_point(left, right_end),
-                squared_distance_to_point(right, left_start),
-                squared_distance_to_point(right, left_end))
+    return (min(_squared_distance_to_point(left, right_start),
+                _squared_distance_to_point(left, right_end),
+                _squared_distance_to_point(right, left_start),
+                _squared_distance_to_point(right, left_end))
             if segments_relationship(left, right) is SegmentsRelationship.NONE
             else 0)
 
 
-def squared_distance_to_point(segment: Segment, point: Point) -> Coordinate:
+def _squared_distance_to_point(segment: Segment, point: Point) -> Coordinate:
     start, end = segment
     factor = max(0, min(1, _robust_divide(projection.signed_length(
             start, point, start, end),
@@ -88,9 +94,3 @@ def squared_distance_to_point(segment: Segment, point: Point) -> Coordinate:
 def _squared_points_distance(left: Point, right: Point) -> Coordinate:
     (left_x, left_y), (right_x, right_y) = left, right
     return (left_x - right_x) ** 2 + (left_y - right_y) ** 2
-
-
-def _robust_divide(dividend: Coordinate, divisor: Coordinate) -> Coordinate:
-    return (Fraction(dividend, divisor)
-            if isinstance(dividend, int)
-            else dividend / divisor)
