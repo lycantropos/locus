32a524472011442bb841f42389e1cf72f8d41ac1
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-14 01:11:25 +0300

    Complete tests

--- tests/core_tests/hilbert_tests/strategies.py
+++
@@ -1,5 +0,0 @@
-from hypothesis import strategies
-
-from locus.core.hilbert import SQUARE_SIZE
-
-hilbert_coordinates = strategies.integers(0, SQUARE_SIZE - 1)
--- tests/core_tests/utils_tests/strategies.py
+++
@@ -1,4 +0,0 @@
-from hypothesis import strategies
-
-integers = strategies.integers()
-non_zero_integers = strategies.integers(max_value=-1) | strategies.integers(1)
