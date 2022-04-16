d7c2d26bff0290503371a356244a3e22f6261737
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-16 13:49:01 +0700

    Drop 'Python3.5' support

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ locus
 [![](https://img.shields.io/github/license/lycantropos/locus.svg)](https://github.com/lycantropos/locus/blob/master/LICENSE "License")
 [![](https://badge.fury.io/py/locus.svg)](https://badge.fury.io/py/locus "PyPI")
 
-In what follows `python` is an alias for `python3.5` or `pypy3.5`
-or any later version (`python3.6`, `pypy3.6` and so on).
+In what follows `python` is an alias for `python3.6` or `pypy3.6`
+or any later version (`python3.7`, `pypy3.7` and so on).
 
 Installation
 ------------
--- setup.py
+++ setup.py
@@ -7,17 +7,21 @@ import locus
 
 project_base_url = 'https://github.com/lycantropos/locus/'
 
+
+def read_file(path_string: str) -> str:
+    return Path(path_string).read_text(encoding='utf-8')
+
+
 setup(name=locus.__name__,
       packages=find_packages(exclude=('tests', 'tests.*')),
       version=locus.__version__,
       description=locus.__doc__,
-      long_description=Path('README.md').read_text(encoding='utf-8'),
+      long_description=read_file('README.md'),
       long_description_content_type='text/markdown',
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
       classifiers=[
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
@@ -27,5 +31,5 @@ setup(name=locus.__name__,
       license='MIT License',
       url=project_base_url,
       download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.5',
-      install_requires=Path('requirements.txt').read_text(encoding='utf-8'))
+      python_requires='>=3.6',
+      install_requires=read_file('requirements.txt'))
