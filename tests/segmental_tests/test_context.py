e5978c16ea7903ef503807f7dc835ab3eb800420
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-16 14:08:36 +0700

    Add test for 'segmental.Tree.context' property

---
+++ tests/segmental_tests/test_context.py
@@ -0,0 +1,12 @@
+from ground.base import Context
+from hypothesis import given
+
+from locus.segmental import Tree
+from . import strategies
+
+
+@given(strategies.trees)
+def test_basic(tree: Tree) -> None:
+    result = tree.context
+
+    assert isinstance(result, Context)
